07af0051f68207951a7fcdc9da40df7057974f87
roiang <thebancare@gmail.com>
2022-04-29 18:16:31 -0500

    ehhh?
    ehhhh?

--- firebase-get-to-know-flutter/step_02/lib/main.dart
+++ firebase-get-to-know-flutter/step_02/lib/main.dart
@@ -1,12 +1,26 @@
+<<<<<<< Updated upstream
 //many errors wtf is this  whare are my commits? in this proyect can they readme?
 
+=======
+import 'package:firebase_auth/firebase_auth.dart'; //new
+import 'package:firebase_core/firebase_core.dart'; //new
+>>>>>>> Stashed changes
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:provider/provider.dart'; //new
+
+// import 'firebase_options.dart';              // new
+import 'src/authentication.dart';            // new
 import 'src/widgets.dart';
 
 void main() {
-  runApp(App());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ApplicationState(),
+      builder: (context, _) => App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
@@ -27,7 +41,7 @@ class App extends StatelessWidget {
       home: const HomePage(),
     );
   }
-}
+}  
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -44,6 +58,19 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 8),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
+          //aplicaremos el Consumer applicationState , crearemos el authentication widget 
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Authentication(
+              email: appState.email,
+              loginState: appState.loginState,
+              startLoginFlow: appState.startLoginFlow,
+              verifyEmail: appState.verifyEmail,
+              signInWithEmailAndPassword: appState.signInWithEmailAndPassword,
+              cancelRegistration: appState.cancelRegistration,
+              registerAccount: appState.registerAccount,
+              signOut: appState.signOut,
+            ),
+          ),
           const Divider(
             height: 8,
             thickness: 1,
@@ -58,5 +85,96 @@ class HomePage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class ApplicationState extends ChangeNotifier {
+  
+  ApplicationState(){
+     init();
+  }
+  Future<void> init() async {
+    await Firebase.initializeApp(
+      options: Firebase.apps.first.options,
+    );
+
+
+    FirebaseAuth.instance.userChanges().listen((user) { //aqui se actualiza el usuario 
+      if (user != null) {
+        _loginState = ApplicationLoginState.loggedIn;
+      } else {
+        _loginState = ApplicationLoginState.loggedOut;
+      }
+      notifyListeners();
+    });
+  }
+
+  ApplicationLoginState _loginState = ApplicationLoginState.loggedOut;
+  ApplicationLoginState get loginState => _loginState;
+  String? _email;
+  String? get email => _email;
+  void startLoginFlow() {
+      _loginState = ApplicationLoginState.emailAddress;
+      notifyListeners(); 
+      }
+
+  Future<void> verifyEmail( //verificacion de correo
+    String email,
+    void Function(FirebaseAuthException e) errorCallback,
+  ) async {
+    try {
+      var methods =
+          await FirebaseAuth.instance.fetchSignInMethodsForEmail(email); //metodo de verificacion de el email
+      if (methods.contains('password')) {
+        _loginState = ApplicationLoginState.password; //se autentifica la contraseña 
+      } else {
+        _loginState = ApplicationLoginState.register; //re registra el usuario
+          //esto es importante porque esta es la manera en como esta configurada de manera austera la verificacion por el proceso de un paso a otro cuando es evidente que se puede hacer de otra mnera esto esta hecho para que del poner el correo y no esta inscrito se registre como nuevo y crea un nuevo usuario en la base de datos
+      }
+      _email = email;
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      errorCallback(e);
+    }
+  }
+
+  Future<void> signInWithEmailAndPassword( //Ingreso de Ususario
+    String email,
+    String password,
+    void Function(FirebaseAuthException e) errorCallback,
+  ) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword( //metodo de login
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      errorCallback(e); //aqui el usuario puede intentar registrarse o ingresar nuevamente 
+    }
+  }
+
+  void cancelRegistration() { //cancela el registro 
+    _loginState = ApplicationLoginState.emailAddress;
+    notifyListeners();
+  }
+
+
+    Future<void> registerAccount( //registra el usuario creacion del usuario
+      String email,
+      String displayName,
+      String password,
+      void Function(FirebaseAuthException e) errorCallback) async {
+    try {
+      var credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      await credential.user!.updateDisplayName(displayName);
+    } on FirebaseAuthException catch (e) {
+      errorCallback(e);
+    }
+  }
+
+
+  void signOut() {
+    FirebaseAuth.instance.signOut();
   }
 }
